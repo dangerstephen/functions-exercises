4abe6e4f7f58dae6854a2c4e5f684990e5cbe02e
DANGERSTEPHEN <DANGERSTEVE1@GMAILCOM>
2016-10-27 22:20:26 -0700

    created a few functions

--- functions.js
+++ functions.js
@@ -1,1 +1,60 @@
-// TODO: add your solutions here!
+function combineWords(word1, word2) {
+    return word1+word2;
+}
+
+var result = combineWords('light', 'house');
+console.log(result);
+// displays 'lighthouse'
+
+
+//-------------------------------
+
+function repeatPhrase(phrase, n) {
+	for(var i = 0; i < n; i++) {
+		console.log(phrase);
+	}
+}
+repeatPhrase ("Hello", 5);
+// displays
+// Hello
+// Hello
+// Hello
+// Hello
+// Hello
+
+//-------------------------------
+
+function toTheNthPower(number, power) {
+    // TODO: Place your code here
+}
+
+var result = toTheNthPower(4, 5);
+console.log(result);
+// displays 1024
+
+//-------------------------------
+
+
+
+function areaOfACircle(radius) {
+    // TODO: Place your code here
+}
+
+var result = areaOfACircle(2);
+console.log(result);
+// displays approximately 12.57
+
+
+
+
+//-------------------------------
+
+function pythagoreanTheorem(a, b) {
+    // TODO: Place your code here
+}
+
+var result = pythagoreanTheorem(3, 4);
+console.log(result);
+// should display 5;
+
+//-------------------------------
